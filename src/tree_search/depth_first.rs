8c60d5f599e218d3b9b6ff0da86b76718acc8a15
Anonymous <anonymous@nucarch>
2021-03-08 10:07:40 -0500

    Changing the depth first algorithm.

--- src/tree_search/depth_first.rs
+++ src/tree_search/depth_first.rs
@@ -7,36 +7,41 @@ where
     T: Environment<Action, AgentId> + Copy + Clone,
 {
     if env.is_terminal() {
-        match env.winner() {
-            Some(a) => {
-                if a == *agent {
-                    return 1.0;
-                } else {
-                    return -1.0;
-                }
-            }
-            None => return 0.0,
-        }
+        return terminal_score(env, agent);
     } else {
         let actions = env.valid_actions();
         let is_agent_turn = env.turn() == *agent;
 
-        let init_value = if is_agent_turn {
-            f64::NEG_INFINITY
-        } else {
-            f64::INFINITY
-        };
-
         // For each action, performs the action, calculates the value of the new environment,
         // and maximizes or minimizes that value depending on whether it is the turn of the player or not.
         let next_env = actions
-            .map(|x| env.what_if(&x))
-            .map(|x| depth_first(&x, agent))
-            .fold(
-                init_value,
-                |a: f64, b: f64| if is_agent_turn { a.max(b) } else { a.min(b) },
-            );
+            .map(|x| depth_first(&env.what_if(&x), agent))
+            .max_by(|a, b| {
+                if is_agent_turn {
+                    a.partial_cmp(&b).expect("Trying to compare with NaN")
+                } else {
+                    b.partial_cmp(&a).expect("Trying to compare with NaN")
+                }
+            });
 
-        return next_env;
+        return next_env.unwrap();
+    }
+}
+
+/// Finds score for terminal environments
+fn terminal_score<Action, AgentId, T>(env: &T, agent: &AgentId) -> f64
+where
+    AgentId: Eq,
+    T: Environment<Action, AgentId> + Copy + Clone,
+{
+    match env.winner() {
+        Some(a) => {
+            if a == *agent {
+                1.0
+            } else {
+                -1.0
+            }
+        }
+        None => 0.0,
     }
 }
