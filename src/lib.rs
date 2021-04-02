88578b8befae360d880300274981bcd5769ada1e
Anonymous <anonymous@nucarch>
2021-04-02 16:34:40 -0400

    erasing files moved to a different crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
 pub mod abstractions;
 pub mod agents;
 pub mod cache;
-pub mod tictactoe;
 pub mod tree_search;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- src/tictactoe/mod.rs
+++
@@ -1,8 +0,0 @@
-/// Actions will be a number from 0 to 8 representing the position on the tic tac toe board.
-pub type Action = u8;
-
-mod agentid;
-pub use self::agentid::AgentId;
-
-mod board;
-pub use self::board::Board;
