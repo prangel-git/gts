3d9532425a841628dac8211adcd2fbcd9dbcfa46
Anonymous <anonymous@nucarch>
2021-04-02 17:42:04 -0400

    Formatting.

--- src/agents/mod.rs
+++ src/agents/mod.rs
@@ -1,10 +1,9 @@
-mod human_agent;
-mod minmax_agent;
 mod alphabeta_agent;
+mod human_agent;
 mod mcts_utc_agent;
+mod minmax_agent;
 
-
-pub use human_agent::HumanPlayer;
-pub use minmax_agent::MinmaxAgent;
 pub use alphabeta_agent::AlphabetaAgent;
+pub use human_agent::HumanPlayer;
 pub use mcts_utc_agent::MctsUctAgent;
+pub use minmax_agent::MinmaxAgent;
