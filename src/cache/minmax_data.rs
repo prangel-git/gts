692bef65d2490dc38277bb929c24683529c888bd
Anonymous <anonymous@nucarch>
2021-05-26 10:36:36 -0400

    adding structure to keep data for min max algorithm.

---
+++ src/cache/minmax_data.rs
@@ -0,0 +1,27 @@
+pub struct MinMaxData<Action> {
+    pub is_maximizer: bool,
+    pub depth: usize,
+    pub value: f64,
+    pub action: Option<Action>,
+}
+
+impl<Action> MinMaxData<Action>
+{
+    pub fn new(is_maximizer: bool) -> Self {
+        if is_maximizer {
+            MinMaxData{
+                is_maximizer: true,
+                depth:0,
+                value: f64::NEG_INFINITY,
+                action: None,
+            }
+        } else {
+            MinMaxData{
+                is_maximizer: false,
+                depth: 0,
+                value: f64::INFINITY,
+                action: None,
+            }
+        }
+    }
+}
