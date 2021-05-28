ae37a7d95dfe3041a20b04d3c9fbeefb7704d3be
Anonymous <anonymous@nucarch>
2021-05-28 12:12:11 -0400

    Everything ready to move things to new cache.

--- src/cache/node.rs
+++ src/cache/node.rs
@@ -6,7 +6,7 @@ use std::rc::Rc;
 use crate::abstractions::Environment;
 
 use super::minmax_data::MinMaxData;
-use super::utils::get_or_insert;
+// use super::utils::get_or_insert;
 use super::utils::node_partial_cmp;
 
 pub type NodeRRMM<T, Action, AgentId> = NodeRR<T, Action, AgentId, MinMaxData<Action>>;
@@ -85,7 +85,9 @@ where
             match self.to_visit.next() {
                 Some(a) => {
                     let env_next = Rc::new(self.env.what_if(&a));
-                    let node_next_ptr = get_or_insert(&env_next, self.cache_ptr.clone());
+                    // let node_next_ptr = get_or_insert(&env_next, self.cache_ptr.clone());
+                    let next_node = Self::with_cache(&env_next, self.cache_ptr.clone());
+                    let node_next_ptr = Rc::new(RefCell::new(next_node));
                     let output = (node_next_ptr, a);
                     self.visited.push(output.clone());
                     self.index += 1;
